3665a0d9fedc37a037ad24033c93c7810d61d9a7
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-02-09 23:07:10 -0200

    Including starting bot

---
+++ src/firstBot.js
@@ -0,0 +1,21 @@
+const env = require('../.env');
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(env.token)
+
+bot.start(ctx => {
+    const from = ctx.update.message.from
+    console.log(from)
+    ctx.reply(`Welcome, ${from.first_name}`)
+})
+
+bot.on('text', async (ctx, next) => {
+    await ctx.reply('First middleware')
+    //next()
+})
+
+bot.on('text', async ctx => {
+    await ctx.reply('Second middleware')
+})
+
+bot.startPolling()
