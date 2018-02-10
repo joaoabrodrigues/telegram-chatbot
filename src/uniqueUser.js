1781a4e3846d2a9e9e53ad58b9d8a775cd07e05a
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-02-09 23:13:06 -0200

    Including unique user bot

---
+++ src/uniqueUser.js
@@ -0,0 +1,16 @@
+const env = require('../.env');
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(env.token)
+
+bot.start(ctx => {
+    const from = ctx.update.message.from
+    console.log(from)
+    if (from.id === 1) {
+        ctx.reply('Ao seu dispor, mestre!')
+    } else {
+        ctx.reply('Sinto muito, mas eu só falo com meu mestre!')
+    }
+})
+
+bot.startPolling()
