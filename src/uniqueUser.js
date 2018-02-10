6ee87b9804612d8258639f5c3c8fa7ecf2fd84ff
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-02-09 23:21:39 -0200

    Updating my user id

--- src/uniqueUser.js
+++ src/uniqueUser.js
@@ -6,7 +6,7 @@ const bot = new Telegraf(env.token)
 bot.start(ctx => {
     const from = ctx.update.message.from
     console.log(from)
-    if (from.id === 1) {
+    if (from.id === env.myId) {
         ctx.reply('Ao seu dispor, mestre!')
     } else {
         ctx.reply('Sinto muito, mas eu só falo com meu mestre!')
