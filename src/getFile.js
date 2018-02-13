acb2d8932e12234752655d77677a3bcc1f60fa15
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-02-13 00:03:16 -0200

    obtaining file from telegram api

---
+++ src/getFile.js
@@ -0,0 +1,24 @@
+const env = require('../.env');
+const Telegraf = require('telegraf')
+const schedule = require('node-schedule')
+const axios = require('axios');
+
+const bot = new Telegraf(env.token)
+
+bot.on('voice', async ctx => {
+    const id = ctx.update.message.voice.file_id
+
+    await axios.get(`${env.apiUrl}/getFile?file_id=${id}`).then(resp => {
+        ctx.replyWithVoice({ url: `${env.apiFileUrl}/${resp.data.result.file_path}` })
+    })
+})
+
+bot.on('photo', async ctx =>{
+    const id = ctx.update.message.photo[0].file_id
+
+    await axios.get(`${env.apiUrl}/getFile?file_id=${id}`).then(resp => {
+        ctx.replyWithPhoto({ url: `${env.apiFileUrl}/${resp.data.result.file_path}`})
+    })
+})
+
+bot.startPolling()
