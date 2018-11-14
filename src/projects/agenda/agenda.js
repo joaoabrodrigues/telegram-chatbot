9868a956f6f4ebb91d9a61a7957c640e23cf6eec
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-11-13 23:41:01 -0200

    finishing agenda bot

--- src/projects/agenda/agenda.js
+++ src/projects/agenda/agenda.js
@@ -1,9 +1,25 @@
 const env = require('../../../.env')
+
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
+
 const moment = require('moment')
-const { getAgenda, getTask } = require('./agenda.services')
+
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+
+const { getAgenda, 
+        getTask,
+        getTasks,
+        getDone,
+        addTask,
+        finishTask,
+        deleteTask,
+        updateTaskDate,
+        updateTaskNote
+ } = require('./agenda.services')
 
 const bot = new Telegraf(env.token)
 
@@ -18,10 +34,10 @@ const showTask = async (ctx, taskId, newMsg = false) => {
     const task = await getTask(taskId)
     const conclusion = task.conclusion_date ? `\n<b>Finished in:</b> ${formatDate(task.conclusion_date)}` : ''
     const msg = `
-        <b>${task.description}</b>
-        <b>Expected:</b> ${formatDate(task.expected_date)}
-        <b>Obs:</b> ${task.obs}
-    `
+    <b>${task.description}</b>
+<b>Expected:</b> ${formatDate(task.expected_date)}${conclusion}
+<b>Note:</b> ${task.note || ''}`
+
     if (newMsg) {
         ctx.reply(msg, taskButtons(taskId))
     } else {
@@ -40,7 +56,7 @@ const agendaButtons = tasks => {
 const taskButtons = taskId => Extra.HTML().markup(Markup.inlineKeyboard([
     Markup.callbackButton('✔️', `finish ${taskId}`),
     Markup.callbackButton('📅', `setDate ${taskId}`),
-    Markup.callbackButton('💬', `addObs ${taskId}`),
+    Markup.callbackButton('💬', `addNote ${taskId}`),
     Markup.callbackButton('✖️', `delete ${taskId}`)
 ], { columns: 4 }))
 
@@ -51,8 +67,141 @@ bot.command('day', async ctx => {
     ctx.reply(`Here is your day agenda`, agendaButtons(tasks))
 })
 
+bot.command('tomorrow', async ctx => {
+    const tasks = await getAgenda(moment().add({ day: 1 }))
+    ctx.reply(`Here is your tasks until tomorrow`, agendaButtons(tasks))
+})
+
+bot.command('week', async ctx => {
+    const tasks = await getAgenda(moment().add({ week: 1 }))
+    ctx.reply(`Here is your tasks until next week`, agendaButtons(tasks))
+})
+
+bot.command('done', async ctx => {
+    const tasks = await getDone()
+    ctx.reply(`Here is your done tasks`, agendaButtons(tasks))
+})
+
+bot.command('tasks', async ctx => {
+    const tasks = await getTasks()
+    ctx.reply(`Here is your tasks with no date`, agendaButtons(tasks))
+})
+
 bot.action(/show (.+)/, async ctx => {
     await showTask(ctx, ctx.match[1])
+})
+
+bot.action(/finish (.+)/, async ctx => {
+    await finishTask(ctx.match[1])
+    await showTask(ctx, ctx.match[1])
+    await ctx.reply('Task finished')
+})
+
+bot.action(/delete (.+)/, async ctx => {
+    await deleteTask(ctx.match[1])
+    await ctx.editMessageText('Task deleted')
+})
+
+const keyboardDate = Markup.keyboard([
+    ['Today', 'Tomorrow'],
+    ['1 Week', '1 Month']
+]).resize().oneTime().extra()
+
+let taskId = null
+
+//----------- Date Scene
+
+const dateScene = new Scene('date')
+
+dateScene.enter(ctx => {
+    taskId = ctx.match[1]
+    ctx.reply('Would you like do set a date?', keyboardDate)
+})
+
+dateScene.leave(ctx => taskId = null)
+
+dateScene.hears(/today/gi, async ctx => {
+    const date = moment()
+    handleDate(ctx, date)
+})
+
+dateScene.hears(/tomorrow/gi, async ctx => {
+    const date = moment().add({ days: 1 })
+    handleDate(ctx, date)
+})
+
+dateScene.hears(/^(\d+) day(s)?$/gi, async ctx => {
+    const date = moment().add({ days: ctx.match[1] })
+    handleDate(ctx, date)
+})
+
+dateScene.hears(/^(\d+) week(s)?$/gi, async ctx => {
+    const date = moment().add({ weeks: ctx.match[1] })
+    handleDate(ctx, date)
+})
+
+dateScene.hears(/^(\d+) month(s)?$/gi, async ctx => {
+    const date = moment().add({ months: ctx.match[1] })
+    handleDate(ctx, date)
+})
+
+dateScene.hears(/(\d{2}\/\d{2}\/\d{4})/g, async ctx => {
+    const date = moment(ctx.match[1], 'DD-MM-YYYY')
+    handleDate(ctx, date)
+})
+
+const handleDate = async (ctx, date) => {
+    await updateTaskDate(taskId, date)
+    await ctx.reply('Date updated!')
+    await showTask(ctx, taskId, true)
+    ctx.scene.leave()
+}
+
+dateScene.on('message', ctx => {
+    ctx.reply('Accepted patterns\ndd/MM/yyyy\nX days\nX weeks\nX months')
+})
+
+//----------- Note Scene
+
+const noteScene = new Scene('note')
+
+noteScene.enter(ctx => {
+    taskId = ctx.match[1]
+    ctx.reply('Enter your note...')
+})
+
+noteScene.leave(ctx => taskId = null)
+
+noteScene.on('text', async ctx => {
+    const task = await getTask(taskId)
+    const newNote = ctx.update.message.text
+    const note = task.note ? task.note + '\n---\n' + newNote : newNote
+    const res = await updateTaskNote(taskId, note)
+    await ctx.reply('Note added!')
+    await showTask(ctx, taskId, true)
+    ctx.scene.leave()
+})
+
+noteScene.on('message', ctx => ctx.reply('Only text notes are allowed!'))
+
+//---------------- SET STAGES
+
+const stage = new Stage([dateScene, noteScene])
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.action(/setDate (.+)/, Stage.enter('date'))
+bot.action(/addNote (.+)/, Stage.enter('note'))
+
+//---------------- ADD TASK
+
+bot.on('text', async ctx => {
+    try {
+        const task = await addTask(ctx.update.message.text)
+        await showTask(ctx, task.id, true)
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 bot.startPolling()
--- src/projects/agenda/agenda.services.js
+++ src/projects/agenda/agenda.services.js
@@ -15,4 +15,51 @@ const getTask = async id => {
     return res.data
 }
 
-module.exports = { getAgenda, getTask }
+const getTasks = async () => {
+    const res = await axios.get(`${baseUrl}?_sort=description&_order=asc`)
+    return res.data.filter(item => item.expected_date === null && item.conclusion_date === null)
+}
+
+const getDone = async () => {
+    const res = await axios.get(`${baseUrl}?_sort=conclusion_date,description&_order=asc`)
+    return res.data.filter(item => item.conclusion_date !== null)
+}
+
+const addTask = async desc => {
+    const res = await axios.post(`${baseUrl}`, { description: desc, conclusion_date: null, expected_date: null, note: null})
+    return res.data
+}
+
+const finishTask = async id => {
+    const task = await getTask(id)
+    const res = await axios.put(`${baseUrl}/${id}`, { ...task, conclusion_date: moment().format('YYYY-MM-DD') })
+    return res.data
+}
+
+const deleteTask = async id => {
+    await axios.delete(`${baseUrl}/${id}`)
+}
+
+const updateTaskDate = async (id, date) => {
+    const task = await getTask(id)
+    const res = await axios.put(`${baseUrl}/${id}`, { ...task, expected_date: date.format('YYYY-MM-DD')})
+    return res.data
+}
+
+const updateTaskNote = async (id, note) => {
+    const task = await getTask(id)
+    const res = await axios.put(`${baseUrl}/${id}`, { ...task, note: note})
+    return res.data
+}
+
+module.exports = { 
+    getAgenda, 
+    getTask,
+    getTasks,
+    getDone,
+    addTask,
+    finishTask,
+    deleteTask,
+    updateTaskDate,
+    updateTaskNote
+ }
